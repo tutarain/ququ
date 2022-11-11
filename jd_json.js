8a54c244f4f884c299e7f99d726b74dfae95928f
tutarain <117993289+tutarain@users.noreply.github.com>
2022-11-11 21:58:32 +0800

    Update jd_json.js

--- jd_json.js
+++ jd_json.js
@@ -9,7 +9,7 @@ author      ddgksf2013
 [rewrite_local]
 
 ^https?:\/\/api\.m\.jd\.com\/client\.action\?functionId=hotWords url script-response-body https://github.com/tutarain/ququ/main/jd_json.js
-^https?:\/\/api\.m\.jd\.com\/client\.action\?functionId=hotSearchTerms url script-response-body https://github.com/dtutarain/ququ/main/jd_json.js
+^https?:\/\/api\.m\.jd\.com\/client\.action\?functionId=hotSearchTerms url script-response-body https://github.com/tutarain/ququ/main/jd_json.js
 
 [mitm]
 
