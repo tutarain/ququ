31b37cf1cef0204357dc5c4365391d073b482c8d
tutarain <117993289+tutarain@users.noreply.github.com>
2022-11-11 21:47:31 +0800

    Update alidrive.js

--- alidrive.js
+++ alidrive.js
@@ -2,12 +2,6 @@
 
 > 应用名称：阿里云盘
 > 脚本作者：Cuttlefish
-> 微信账号：墨鱼手记
-> 更新时间：2022-10-16
-> 通知频道：https://t.me/ddgksf2021
-> 投稿助手：https://t.me/ddgksf2013_bot
-> 脚本功能：优化首页display
-> 问题反馈：📮 ddgksf2013@163.com 📮
 > 特别说明：⛔⛔⛔
             本脚本仅供学习交流使用，禁止转载售卖
             ⛔⛔⛔
@@ -17,7 +11,7 @@
 [rewrite_local]
 
 # ～ 阿里云盘（2022-10-16）@ddgksf2013
-^https?:\/\/api\.aliyundrive\.com\/apps\/v\d\/users\/apps\/widgets$ url script-response-body https://github.com/ddgksf2013/Cuttlefish/raw/master/Script/alidrive.js
+^https?:\/\/api\.aliyundrive\.com\/apps\/v\d\/users\/apps\/widgets$ url script-response-body https://github.com/tutarain/ququ/main/alidrive.js
 
 [mitm]
 
