06a205caa7a255a6156500106690e68b436c10f7
tutarain <117993289+tutarain@users.noreply.github.com>
2022-11-12 14:17:03 +0800

    Update revenuecat.js

--- revenuecat.js
+++ revenuecat.js
@@ -1,20 +1,21 @@
-re('"expires_date":"\\w{4}@"period_type":"\\w+"','"expires_date":"2099@"period_type":"active"');
-function re() {
-var body = $response.body;
-if(!body){
-$done({});
-}
- if (arguments[0].includes("@")) {
-  var regs = arguments[0].split("@");
-  var strs = arguments[1].split("@");
-  for (i = 0;i < regs.length;i++) {
-   var reg = new RegExp(regs[i],"g");
-   body = body.replace(reg, strs[i]);
- }
-}
- else {
-  var reg = new RegExp(arguments[0],"g");
-  body = body.replace(reg, arguments[1]);
-}
- $done({body});
-} 
+/***********************************
+
+> 应用名称：RevenueCat多合一脚本
+> 脚本作者：Cuttlefish
+> 解锁应用：如下所示（共计31个）陆续更新...
+
+Pixelmator、Planny、Gear、图图记账、Aphrodite、Apollo、MoneyThings、目标地图、Audiomack、
+1Blocker、Scanner Pro、Darkroom、谜底时钟、Pillow、VSCO、Grow、WhiteCloud、Fin、奇妙组件、
+Widgetsmith、vision、Percento、Airmail、Usage、pdfviewer、Spark、谜底黑胶、ColorWidgets、
+手机硬件管家、WidgetArt、卡片馆
+
+[rewrite_local]
+
+# ～ RevenueCat（2022-11-12）@ddgksf2013
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/(\$RCAnonymousID%)?(\w)*$) url script-echo-response https://codeberg.org/ddgksf2013/Cuttlefish/raw/branch/master/Crack/revenuecat.js
+
+[mitm]
+
+hostname=api.revenuecat.com
+
+***********************************/
